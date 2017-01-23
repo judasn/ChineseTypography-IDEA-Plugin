463d3fbfe073a68d22a641608d13294944c6aa55
judasn <jn3.141592654@gmail.com>
2017-01-23 17:47:32 +0800

    2017-01-23在分割之前先进行空格/制表符处理

--- src/com/youmeek/typography/TransitionAction.java
+++ src/com/youmeek/typography/TransitionAction.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
+import com.youmeek.typography.utils.RegexExpressionUtils;
 import com.youmeek.typography.utils.TransitionUtils;
 import org.apache.commons.lang.StringUtils;
 
@@ -15,7 +16,7 @@ import org.apache.commons.lang.StringUtils;
  * http://www.YouMeek.com
  * admin@youmeek.com
  * 微信公众号：iYouMeek
- * 我常用 IntelliJ IDEA 进行写作，开发此插件可以快速对文章内容的中文、英文、符号之间增加空格，增加文章可读性。
+ * 自己常在 IntelliJ IDEA 中写博文，对于中英文之间的排版，觉得有一个空格后可读性会更加强，顾开发此插件。
  * 本插件的核心转换类是来自 pangu，自己做了小修改。在此表示感谢：https://github.com/vinta/pangu.java
  */
 public class TransitionAction extends AnAction {
@@ -32,12 +33,12 @@ public class TransitionAction extends AnAction {
 			return;
 		}
 
-
 		//下面这句话也可以使用 lambda 语法写：Runnable runnable = () -> editor.getDocument().replaceString(selectionModel.getSelectionStart(), selectionModel.getSelectionEnd(), TransitionUtils.spacingText(selectedText));
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
-				editor.getDocument().replaceString(selectionModel.getSelectionStart(), selectionModel.getSelectionEnd(), TransitionUtils.spacingText(selectedText));
+				//先对空格、制表符进行换掉，然后再重新进行分隔处理。其中分行不处理，因为在写文章的时候复制的一些内容分行是有意义的。
+				editor.getDocument().replaceString(selectionModel.getSelectionStart(), selectionModel.getSelectionEnd(), TransitionUtils.spacingText(RegexExpressionUtils.replace(selectedText, "\\f|\\r|\\t", "")));
 			}
 		};
 
